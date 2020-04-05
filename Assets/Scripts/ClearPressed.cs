00c51d23ae5d4121f2e6088eb0445d40cdfceedf
Jay Montal <jmontal@cisco.com>
2020-04-05 15:36:09 -0600

    Add's smooth line rendering, draw/play buttons, player model, gravity, and collision

---
+++ Assets/Scripts/ClearPressed.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+ 
+public class ClearPressed : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+ 
+	public bool buttonPressed;
+	TrailScript script;
+
+	GameObject Renderer;
+	bool test;
+
+	void Start () {
+		Renderer = GameObject.FindGameObjectsWithTag("renderer")[0];
+		script = Renderer.GetComponent<TrailScript>();
+	}
+
+	public void OnPointerDown(PointerEventData eventData){
+	    buttonPressed = true;
+	    script.playPressed = true;
+	}
+	 
+	public void OnPointerUp(PointerEventData eventData){
+	    buttonPressed = false;
+		script.playPressed = false;
+	}
+}
---
+++ Assets/Scripts/TrailScript.cs
@@ -0,0 +1,76 @@
+﻿/*
+Script for Line
+Credit to Shivang Chopra.
+https://heartbeat.fritz.ai/the-subtle-art-of-making-lines-in-augmented-reality-using-arcore-and-unity3d-e26718dffa03
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailScript : MonoBehaviour {
+
+	public GameObject lineObject;
+	public GameObject playerObject;
+	Rigidbody rigidBody;
+
+	public GameObject Player;
+	public GameObject camera;
+	public List<GameObject> Points = new List<GameObject>();
+	public bool buttonPressed = false;
+	public bool playPressed = false;
+
+	bool playerVisible = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(buttonPressed) {
+			Debug.Log("Touched");
+			Vector3 camPos = camera.transform.position;
+ 			Vector3 camDirection = camera.transform.forward;
+ 			Quaternion camRotation = camera.transform.rotation;
+ 			float spawnDistance = 0.5f;
+			Debug.Log("Touched"+camPos.x+" "+camPos.y+" "+camPos.z);
+ 			Vector3 spawnPos = camPos + (camDirection * spawnDistance);
+			GameObject cur = Instantiate(lineObject, spawnPos,  camRotation);
+
+			if(!playerVisible){
+				playerVisible = true;
+				Vector3 playerPos = spawnPos;
+				playerPos.y = spawnPos.y + 0.1f;
+				Instantiate(playerObject, playerPos, camRotation);
+			}
+
+			cur.transform.SetParent(this.transform);
+		}
+
+		if(playPressed) {
+			if(playerVisible){
+				Player = GameObject.FindGameObjectsWithTag("player")[0];
+				rigidBody = Player.GetComponent<Rigidbody>();
+				rigidBody.constraints &= ~RigidbodyConstraints.FreezePositionY;
+				rigidBody.constraints &= ~RigidbodyConstraints.FreezePositionX;
+			}
+			// GameObject[] spheres = GameObject.FindGameObjectsWithTag("sphere");
+			// int count = spheres.Length;
+
+			// foreach (var sphere in spheres){
+			// 	Destroy(sphere);
+			// }
+
+			// Vector3 camPos = camera.transform.position;
+ 			// Vector3 camDirection = camera.transform.forward;
+ 			// Quaternion camRotation = camera.transform.rotation;
+ 			// float spawnDistance = 0.5f;
+			// Debug.Log("Touched"+camPos.x+" "+camPos.y+" "+camPos.z);
+ 			// Vector3 spawnPos = camPos + (camDirection * spawnDistance);
+			// GameObject cur = Instantiate(lineObject, spawnPos,  camRotation);
+			// cur.transform.SetParent(this.transform);
+		}
+	}
+}
